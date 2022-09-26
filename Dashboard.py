0deff10c8641bb72deee72495729ac264bfcc61f
Abdelhak <anwa.abdel.06@gmail.com>
2022-09-26 16:29:04 +0100

    Version Send From Arduino Yùn To API/DB AND Store Data Need to Fix : Add Only one Sensor with same id

--- Dashboard.py
+++ Dashboard.py
@@ -1,18 +1,38 @@
 # importing the required module
 import matplotlib.pyplot as plt
-import sqlite3
-from sqlite3 import Error
-from pathlib import Path
-from datetime import datetime
+import matplotlib.dates as mdates
+from Base_de_donnée import *
+
 
 def printGraph():
-    with connection_DBase()as conn:
-        c = conn.cursor
-        rSQL = '''SELECT Temperature FROM DataHistory'''
-        c.excute(rSQL)
-        Temperature = conn.fe
+    with connection_DBase() as conn:
+        c = conn.cursor()
+        rSQL = ''' SELECT Temperature FROM DataHistory'''
+        c.execute(rSQL)
+        temp = c.fetchone()
+        rSQL = ''' SELECT Humidity FROM DataHistory'''
+        c.execute(rSQL)
+        hum = c.fetchone()
+        rSQL = ''' SELECT Date FROM DataHistory'''
+        c.execute(rSQL)
+        mdates = c.fetchone()
+
+
+        print(temp[0])
+        print(hum[0])
+        print(str(date[0]))
+        y = [2, 4, 1]
+        plt.plot(temp[0],y)
+        # naming the x axis
+        plt.xlabel('x - time')
+        # naming the y axis
+        plt.ylabel('y - Temperature')
+        plt.title('Dashboard')
+        # function to show the plot
+        plt.show()
+
     return
-# x axis values
+"""# x axis values
 x = [1, 2, 3]
 # corresponding y axis values
 y = [2, 4, 1]
@@ -30,3 +50,5 @@ plt.title('Dashboard')
 
 # function to show the plot
 plt.show()
+"""
+printGraph()
